fa14e42ba31f7d3cd6486c4b136f2b664fa89930
Lurux <lurux-dev@protonmail.com>
2026-03-18 19:29:05 +0100

    Add NavigationPreferences

---
+++ app/src/main/kotlin/com/metrolist/music/constants/NavigationPreferences.kt
@@ -0,0 +1,134 @@
+/**
+ * Metrolist Project (C) 2026
+ * Licensed under GPL-3.0 | See git history for contributors
+ */
+
+package com.metrolist.music.constants
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.runtime.Composable
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.stringPreferencesKey
+import com.metrolist.music.R
+import com.metrolist.music.ui.screens.Screens
+import com.metrolist.music.utils.rememberEnumPreference
+import kotlin.enums.enumEntries
+
+// Max items pinned in navigation bar before items set to "auto" are moved to the top bar
+val MAX_ITEMS_IN_NAV_BAR = 6
+
+enum class NavigationItemPosition {
+    NAV_BAR,
+    TOP_BAR,
+    AUTO,
+    HIDDEN
+}
+
+enum class NavigationItemType {
+    CORE,
+    LIBRARY,
+    OTHER
+}
+
+enum class NavigationPreferences(
+    @StringRes val title: Int,
+    @DrawableRes val iconInactive: Int,
+    @DrawableRes val iconActive: Int,
+    val baseRoute: String,
+    val key: Preferences.Key<String>,
+    val type: NavigationItemType,
+    val default_position: NavigationItemPosition
+) {
+    HOME(
+        R.string.home,
+        R.drawable.home_outlined,
+        R.drawable.home_filled,
+        "home",
+        stringPreferencesKey("nav_home_position"),
+        NavigationItemType.CORE,
+        NavigationItemPosition.NAV_BAR
+    ),
+    SEARCH(
+        R.string.search,
+        R.drawable.search,
+        R.drawable.search,
+        "search_input",
+        stringPreferencesKey("nav_home_position"),
+        NavigationItemType.OTHER,
+        NavigationItemPosition.AUTO
+    ),
+    HISTORY(
+        R.string.history,
+        R.drawable.history,
+        R.drawable.history,
+        "history",
+        stringPreferencesKey("nav_history_position"),
+        NavigationItemType.OTHER,
+        NavigationItemPosition.TOP_BAR
+    ),
+    STATS(
+        R.string.stats,
+        R.drawable.stats,
+        R.drawable.stats,
+        "stats",
+        stringPreferencesKey("nav_stats_position"),
+        NavigationItemType.OTHER,
+        NavigationItemPosition.TOP_BAR
+    ),
+    LISTEN_TOGETHER(
+        R.string.together,
+        R.drawable.group_outlined,
+        R.drawable.group_filled,
+        "listen_together",
+        stringPreferencesKey("nav_listen_together_position"),
+        NavigationItemType.OTHER,
+        NavigationItemPosition.TOP_BAR
+    ),
+    LIBRARY(
+        R.string.filter_library,
+        R.drawable.library_music_outlined,
+        R.drawable.library_music_filled,
+        "library",
+        stringPreferencesKey("nav_library_position"),
+        NavigationItemType.CORE,
+        NavigationItemPosition.NAV_BAR
+    );
+
+    @Composable
+    fun position(): NavigationItemPosition {
+        return rememberEnumPreference(this.key, this.default_position).value
+    }
+
+    @Composable
+    fun shouldShowInNavbar(): Boolean {
+        val position = this.position()
+
+        // True if manually pinned
+        if(position == NavigationItemPosition.NAV_BAR) return true;
+
+        // False if TOP_BAR or HIDDEN
+        if(position != NavigationItemPosition.AUTO) return false;
+
+        // From here on, we can assume we're using "AUTO" position
+        val manualCount = enumEntries<NavigationPreferences>().count {
+            it.position() == NavigationItemPosition.NAV_BAR
+        }
+
+        // More than MAX_ITEMS_IN_NAV_BAR items are manually pinned, don't add to navigation bar
+        if(manualCount >= MAX_ITEMS_IN_NAV_BAR) return false
+
+        // Else, show AUTO items up to MAX_ITEMS_IN_NAV_BAR being visible in navigation bar
+        var count = manualCount
+
+        for(item in NavigationPreferences.entries) {
+            // If we reach ourselves before returning, show
+            if(item == this)                                    return true
+            // An AUTO item before this one is visible
+            if(item.position() == NavigationItemPosition.AUTO)  count++
+            // If > MAX_ITEMS_IN_NAV_BAR before reaching ourselves, don't show
+            if(count >= MAX_ITEMS_IN_NAV_BAR)                   return false
+        }
+
+        throw Exception("Error when evaluating shouldShowInNavbar for item ${this.name}")
+    }
+}
